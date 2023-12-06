5358809d2955d400258514d2a0646fdf872e9b21
Md-Ikhtiar-uddin-howlader <sojib5304@gmail.com>
2023-12-07 03:39:38 +0800

    last_update

--- web/main.dart
+++ web/main.dart
@@ -1,76 +1,117 @@
-import 'dart:html';
 import 'dart:convert';
+import 'dart:html';
 
+// DIMYATI AGUNG NABHAN
 class Product {
-  int itemNumber;
-  String description;
-  double price;
-  int quantity;
-  double total;
+  final String itemNumber;
+  final String description;
+  final double price;
+  final int quantity;
 
   Product({
     required this.itemNumber,
     required this.description,
     required this.price,
     required this.quantity,
-  }) {
-    this.total = calculateTotal();
-  }
+  });
 
-  double calculateTotal() {
-    return price * quantity;
-  }
-
-  double applyDiscount() {
-    // Apply discount for specific product numbers
-    if ([6, 11, 2, 13].contains(itemNumber)) {
-      return total * 0.1; // 10% discount
-    }
-    return 0.0;
-  }
-
+  // Convert Product to a Map
   Map<String, dynamic> toMap() {
     return {
       'itemNumber': itemNumber,
       'description': description,
       'price': price,
       'quantity': quantity,
-      'total': total,
     };
+  }
+
+  // Create a Product from a Map
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      itemNumber: map['itemNumber'],
+      description: map['description'],
+      price: map['price'],
+      quantity: map['quantity'],
+    );
   }
 }
 
 void main() {
-  // Retrieve product data from local storage
+  // AFFIF
+  var products = <Product>[
+    Product(
+      itemNumber: '1',
+      description: 'Iphone 15',
+      price: 4499.00,
+      quantity: 1,
+    ),
+  Product(
+        itemNumber: '2',
+        description: 'Macbook',
+        price: 5999.00,
+        quantity: 1
+      ),
+      Product(
+        itemNumber: '3',
+        description: 'Chrome book',
+        price: 2999.00,
+        quantity: 1
+      ),
+      Product(
+        itemNumber: '4',
+        description: 'Chrome book',
+        price: 2999.00,
+        quantity: 1
+      ),
+      Product(
+        itemNumber: '5',
+        description: 'laptop bag',
+        price: 99.00,
+        quantity: 1
+      ),
+      Product(
+        itemNumber: '6',
+        description: 'Blazers',
+        price: 299.00,
+        quantity: 1
+      ),
+      Product(
+        itemNumber: '7',
+        description: 'Fake Eyelashes',
+        price: 29.00,
+        quantity: 1
+      ),
+      Product(
+        itemNumber: '8',
+        description: 'Car Phone Holders',
+        price: 30.00,
+        quantity: 1
+      ),
+      Product(
+        itemNumber: '9',
+        description: 'Phone Cases',
+        price: 10.00,
+        quantity: 1
+      ),
+      Product(
+        itemNumber: '10',
+        description: 'Headphones',
+        price: 159.00,
+        quantity: 1
+      )
+ 
+  ];
+
+  // Store products in local storage
+  window.localStorage['products_data'] = jsonEncode(products.map((p) => p.toMap()).toList());
+
+  // Retrieve products from local storage
   var storedData = window.localStorage['products_data'];
-  List<Map<String, dynamic>> productsData = [];
-
   if (storedData != null) {
-    productsData = List<Map<String, dynamic>>.from(json.decode(storedData));
-  } else {
-    // Create 15 sample products if no data is stored
-    for (int i = 1; i <= 15; i++) {
-      productsData.add({
-        'itemNumber': i,
-        'description': 'Product $i',
-        'price': 10.99 + i.toDouble(),
-        'quantity': 2 + i,
-      });
-    }
+    var storedProducts = (jsonDecode(storedData) as List<dynamic>).map((e) => Product.fromMap(e)).toList();
 
-    // Save product data to local storage
-    window.localStorage['products_data'] = json.encode(productsData);
+    // Now, storedProducts contains the list of Product objects retrieved from local storage
+    print(storedProducts);
   }
 
-  // Display information about each product
-  for (var productData in productsData) {
-    Product product = Product(
-      itemNumber: productData['itemNumber'],
-      description: productData['description'],
-      price: productData['price'],
-      quantity: productData['quantity'],
-    );
-
-    // product.displayInfo();
-  }
 }
